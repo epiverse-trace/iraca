f65e6b39e67e235e669ab7c29568877865d882cf
jd-otero <julian-otero@outlook.com>
2023-07-18 16:59:40 -0500

    Create utils.cpp for easier sampling

---
+++ src/utils.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <Rcpp.h>
+
+int weightedRandom(std::vector<float> probabilities)
+{
+    int output = 0;
+    std::vector<int> transformedP;
+    int accumulated = 0;
+    for (float i : probabilities)
+    {
+        int transformed = int(i * 1000) + accumulated;
+        transformedP.push_back(transformed);
+        accumulated = transformed;
+    }
+    int randomInteger = Rcpp::sample(1000, 1)[0];
+    // int randomInteger = rand() % 100;
+    for (int i = 0; i < probabilities.size(); i++)
+    {
+        if (randomInteger < transformedP[i])
+        {
+            output = i;
+            break;
+        }
+    }
+    return output;
+};
