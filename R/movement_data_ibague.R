76299e65d87adcc342caf39c67845cc0988235ba
jd-otero <julian-otero@outlook.com>
2023-08-15 14:06:12 -0500

    Add sample data #20

---
+++ R/movement_data_ibague.R
@@ -0,0 +1,7 @@
+#' Movement data sample for testing
+#'
+#' @title movement data sample
+#' @description Movement data for a given municipality in Colombia (DIVIPOLA
+#' code 73001).
+#' @name movement_data_ibague
+"movement_data_ibague"
