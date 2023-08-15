f935fb707e5f13a8496ebfc048328f09d7f9ee20
jd-otero <julian-otero@outlook.com>
2023-08-15 14:06:12 -0500

    Add sample data #20

---
+++ R/demographic_data_ibague.R
@@ -0,0 +1,8 @@
+#' Demographic data sample for testing
+#'
+#' @title demographic data sample
+#' @description Demographic data for a given municipality in Colombia (DIVIPOLA
+#' code 73001) from the national census reported in the MGN.
+#' @name demographic_data_ibague
+#' @source `https://www.dane.gov.co/files/geoportal-provisional/index.html`
+"demographic_data_ibague"
---
+++ R/temperature_data_ibague.R
@@ -0,0 +1,8 @@
+#' Temperature data sample for testing
+#'
+#' @title temperature data sample
+#' @description Temperature data for a given municipality in Colombia (DIVIPOLA
+#' code 73001) between January and June 2023.
+#' @name temperature_data_ibague
+#' @source `http://www.ideam.gov.co/web/tiempo-y-clima`
+"temperature_data_ibague"
