57dc00bc6d7d2022411960d17927cc06bfb377dc
jd-otero <julian-otero@outlook.com>
2023-08-17 14:19:53 -0500

    Update simulation class

---
+++ tests/testthat/test-simulation.R
@@ -0,0 +1,22 @@
+
+ABM_test <- iraca::setup(iraca::demographic_data_ibague, 
+                         iraca::temperature_data_ibague, 
+                         divipola_code = 73001)
+ABM_test_wrong <- unclass(ABM_test)
+
+
+test_that("Simulation errors are thrown", {
+  expect_error(simulation(ABM_test_wrong, 20))
+  expect_error(simulation(ABM_test, 20.5))
+})
+
+simulated_output <- simulation(ABM_test, 20L)
+
+test_that("Simulation works as expected", {
+  expect_s3_class(simulated_output, "data.frame")
+  expect_identical(ncol(simulated_output), 3L)
+  expect_identical(nrow(simulated_output), 20L)
+  expect_type(simulated_output$S, "integer")
+  expect_type(simulated_output$I, "integer")
+  expect_type(simulated_output$R, "integer")
+})
